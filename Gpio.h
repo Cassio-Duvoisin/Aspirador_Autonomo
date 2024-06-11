e230469c3e28676613b44bd92304bf16cd82620d
assisgabriel <47213761+assisgabriel@users.noreply.github.com>
2024-06-11 11:41:48 -0300

    Update Gpio.h

--- Gpio.h
+++ Gpio.h
@@ -1,1 +1,65 @@
+#ifndef GPIO_H
+#define GPIO_H
 
+#define RCC_AHB1ENR_GPIOD_ENABLE		(1<<3)
+#define RCC_AHB1ENR_GPIOD_DISABLED 	(0<<3)
+
+#define RCC_AHB1ENR_GPIOA_ENABLE 		(1<<0)
+#define RCC_AHB1ENR_GPIOA_DISABLED  (0<<0)
+
+#define GPIOD_MODER_PIN12_OUTPUT 	(1<<24)
+#define GPIOD_MODER_PIN13_OUTPUT 	(1<<26)
+#define GPIOD_MODER_PIN14_OUTPUT 	(1<<28)
+#define GPIOD_MODER_PIN15_OUTPUT 	(1<<30)
+
+#define GPIOA_MODER_PIN0_INPUT 		(0<<0)
+
+typedef enum
+{
+	PORTA=0,
+	PORTB,
+	PORTC,
+	PORTD,
+	PORTE,
+	PORTF,
+	PORTG,
+	PORTH,
+	PORTI,
+	PORTJ,
+	PORTK
+} GPIO_PORT;
+
+typedef enum
+{
+	PIN0=0,
+	PIN1,
+	PIN2,
+	PIN3,
+	PIN4,
+	PIN5,
+	PIN6,
+	PIN7,
+	PIN8,
+	PIN9,
+	PIN10,
+	PIN11,
+	PIN12,
+	PIN13,
+	PIN14,
+	PIN15
+} GPIO_PIN;
+
+typedef enum
+{
+	INPUT=0,
+	OUTPUT,
+	ALT,
+	ANALOG
+} GPIO_MODE;
+
+void Gpio_ClkEnable(GPIO_PORT Port);
+void Gpio_ClkDisable(GPIO_PORT Port);
+void Gpio_SetMode(GPIO_PORT Port, GPIO_PIN Pin, GPIO_MODE Mode);
+
+
+#endif /* GPIO_H */
