abba3ac900d457e84042d15a8d66a32798c80899
assisgabriel <47213761+assisgabriel@users.noreply.github.com>
2024-06-11 11:40:47 -0300

    Update LedMgr.h

--- LedMgr.h
+++ LedMgr.h
@@ -1,1 +1,29 @@
+#ifndef LEDMGR_H
+#define LEDMGR_H
 
+typedef enum
+{
+	LED_RED=0,
+	LED_GREEN,
+	LED_BLUE,
+	LED_ORANGE
+} LED_COLOR_TYPE;
+
+typedef enum
+{
+	LED_ON=0,
+	LED_OFF
+} LED_STATUS_TYPE;
+
+typedef struct
+{
+	LED_STATUS_TYPE Status;
+	LED_COLOR_TYPE Color;
+}LED_TYPE;
+
+void LedMgr_TurnLedOn(LED_TYPE Led);
+void LedMgr_TurnLEDoFF(LED_TYPE Led);
+LED_TYPE LedMgr_ConfigLed(LED_COLOR_TYPE Color);
+	
+
+#endif /* LEDMGR_H */
