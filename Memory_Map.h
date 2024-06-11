8d49ca1893253a72e878e1391fb7ba904504a602
assisgabriel <47213761+assisgabriel@users.noreply.github.com>
2024-06-11 11:43:30 -0300

    Update Memory_Map.h

--- Memory_Map.h
+++ Memory_Map.h
@@ -1,1 +1,19 @@
+#ifndef MEMORY_MAP_H
+#define MEMORY_MAP_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define RCC_AHB1ENR		(*((volatile uint32_t *) 0x40023830)) // RCC_AHB1ENR register memory address
+#define GPIOD_MODER		(*((volatile uint32_t *) 0x40020C00)) // RCC_MODER register memory address
+#define GPIOD_BSRR    (*((volatile uint32_t *) 0x40020C18)) // RCC_BSRR  register memory address
+#define GPIOA_MODER 	(*((volatile uint32_t *) 0x40020000))
+#define GPIOA_IDR   	(*((volatile uint32_t *) 0x40020010))
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MEMORY_MAP_H */
