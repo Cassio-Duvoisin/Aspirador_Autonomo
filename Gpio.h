95190951a764ee4712b86d04e301391288c00522
assisgabriel <47213761+assisgabriel@users.noreply.github.com>
2024-06-12 18:56:17 -0300

    Update Gpio.h

--- Gpio.h
+++ Gpio.h
@@ -1,18 +1,7 @@
 #ifndef GPIO_H
 #define GPIO_H
 
-#define RCC_AHB1ENR_GPIOD_ENABLE		(1<<3)
-#define RCC_AHB1ENR_GPIOD_DISABLED 	(0<<3)
-
-#define RCC_AHB1ENR_GPIOA_ENABLE 		(1<<0)
-#define RCC_AHB1ENR_GPIOA_DISABLED  (0<<0)
-
-#define GPIOD_MODER_PIN12_OUTPUT 	(1<<24)
-#define GPIOD_MODER_PIN13_OUTPUT 	(1<<26)
-#define GPIOD_MODER_PIN14_OUTPUT 	(1<<28)
-#define GPIOD_MODER_PIN15_OUTPUT 	(1<<30)
-
-#define GPIOA_MODER_PIN0_INPUT 		(0<<0)
+#include "Memory_Map.h"
 
 typedef enum
 {
