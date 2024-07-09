d6c093c043bc033f31a1771d54901d6ad0cba9bc
assisgabriel <47213761+assisgabriel@users.noreply.github.com>
2024-07-08 23:39:28 -0300

    Update and rename main.h to Hardware_Proxy.h

---
+++ Hardware_Proxy.h
@@ -0,0 +1,9 @@
+#ifndef HARDWARE_PROXY_H
+#define HARDWARE_PROXY_H
+
+#include "Analog.h"
+#include "Digital.h"
+#include "PWM.h"
+#include "LedMgr.h"
+
+#endif /* HARDWARE_PROXY_H */
--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include "Memory_Map.h"
-#include "util.h"
-#include "LedMgr.h"
-#include "Gpio.h"
-
-#endif /* MAIN_H */
