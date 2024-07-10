3057dfeb8ae559dfd7a785cfc222876856a5f458
assisgabriel <47213761+assisgabriel@users.noreply.github.com>
2024-07-09 23:08:15 -0300

    Update Analog.c

--- Analog.c
+++ Analog.c
@@ -1,6 +1,10 @@
 #include "Analog.h"
 
-// Função para inicializar o ADC
+/**
+ * @brief Inicializa o DAC.
+ * 
+ * @param analog Ponteiro para a estrutura Analog_TypeDef que representa o DAC.
+ */
 void Analog_Init(Analog_TypeDef *analog)
 {
     // Configuração do ADC
@@ -16,7 +20,13 @@ void Analog_Init(Analog_TypeDef *analog)
     HAL_ADC_Init(&(analog->hadc));
 }
 
-// Função para ler um canal específico do ADC
+/**
+ * @brief Realiza a leitura de um canal específico do ADC.
+ * 
+ * @param analog Ponteiro para a estrutura Analog_TypeDef que representa o ADC.
+ * @param channel Canal do ADC a ser lido.
+ * @return Valor lido do canal especificado.
+ */
 uint16_t Analog_Read(Analog_TypeDef *analog, uint32_t channel)
 {
     ADC_ChannelConfTypeDef sConfig;
@@ -34,4 +44,16 @@ uint16_t Analog_Read(Analog_TypeDef *analog, uint32_t channel)
     } else {
         return 0;
     }
+}
+
+/**
+ * @brief Escreve um valor analógico no DAC.
+ * 
+ * @param analog Ponteiro para a estrutura Analog_TypeDef que representa o DAC.
+ * @param value Valor a ser escrito no DAC (de 0 a 4095, correspondendo a 0 a Vref).
+ */
+void Analog_Write(Analog_TypeDef *analog, uint16_t value) {
+    HAL_DAC_SetValue(&analog->hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, value);
+    /* DAC_CHANNEL_1 indica o canal 1 do DAC */
+    /* DAC_ALIGN_12B_R indica alinhamento de 12 bits, à direita */
 }
